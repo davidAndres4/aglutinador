1c555f6f832b80b30e258e83ee58b26a5b213d7c
unknown <david.andres.otero@iesjulianmarias.es>
2021-11-17 21:06:45 +0100

    por fin vagit status

--- script_examen.sql
+++ script_examen.sql
@@ -1,24 +1,25 @@
-drop table if exists autor;
-CREATE TABLE IF  NOT  EXISTS autor (
-idAutor	varchar(2)  NOT NULL  PRIMARY KEY,
-nombre	VARCHAR(15),
-apellido1 VARCHAR(15),
-apellido2 VARCHAR(15),
-puesto VARCHAR(35),
-entidadTrabajo varchar(15));
+drop table respuesta;
+drop table pregunta;
+drop table autor;
 
-drop table if exists pregunta;
+CREATE TABLE IF  NOT  EXISTS autor (
+idAutor	varchar(200)  NOT NULL  PRIMARY KEY,
+nombre	VARCHAR(200),
+apellido1 VARCHAR(200),
+apellido2 VARCHAR(200),
+puesto VARCHAR(200),
+entidadTrabajo varchar(200));
+
 CREATE TABLE IF  NOT  EXISTS pregunta(
 idPregunta TINYINT IDENTITY PRIMARY KEY,
-dificultad VARCHAR(10),
-enunciado	VARCHAR(500),
+dificultad VARCHAR(200),
+enunciado	VARCHAR(200),
 idAutor varchar(2) NOT NULL,
 CONSTRAINT FK_idAutor FOREIGN KEY (idAutor) REFERENCES autor (idAutor));
 
-drop table if exists respuesta;
 CREATE TABLE IF  NOT  EXISTS respuesta(
-validez VARCHAR(2),
-contenido VARCHAR(500),
+validez VARCHAR(200),
+contenido VARCHAR(200),
 idPregunta TINYINT,
 CONSTRAINT FK_idPregunta FOREIGN KEY (idPregunta) REFERENCES pregunta (idPregunta));
 
--- src/principal/EjecutableEjAglutinador.java
+++ src/principal/EjecutableEjAglutinador.java
@@ -2,41 +2,74 @@ package principal;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class EjecutableEjAglutinador {
+import clasesJAXB.ObjectFactory;
+import clasesJAXB.TipoAutor;
+import clasesJAXB.TipoAutores;
+import clasesJAXB.TipoExamen;
+import clasesJAXB.TipoPregunta;
+import clasesJAXB.TipoPreguntas;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 
-	public static void main(String[] args) {
-		estableceConexionBBDD();
-		leerScript();
-	
-	}
+public class EjecutableEjAglutinador {
 	
 	static Connection connection;
 	static Statement statement;
 	static Scanner sn;
 
+	public static void main(String[] args) throws JAXBException, SQLException, FileNotFoundException {
+		estableceConexionBBDD();
+		ejecutaScript();
+		leeXML();
+		
+		
+		
+		ResultSet rs;
+		try {
+			rs = statement.executeQuery("select * from autor");
+			while(rs.next()){
+				// read the result set
+				System.out.println(rs.getString(1) + "\t" + rs.getString(2) + " (" + rs.getString(3) + ")");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		connection.close();
+		
+		
+	
+	}
 
 	public static void estableceConexionBBDD() {
 		try{
 			// create a database connection
-			connection = DriverManager.getConnection("jdbc:hsqldb:file:bbdd/examen/examen");
+			connection = DriverManager.getConnection("jdbc:hsqldb:file:bbdd/examen/examenAglutinador");
 			statement = connection.createStatement();
 			
 		}catch(SQLException e){
-			// if the error message is "out of memory",
-			// it probably means no database file is found
 			System.err.println(e.getMessage());
 		}
 	}
 	
-	public static void leerScript() {
+	public static void ejecutaScript() {
 		
 		File archivo = new File("script_examen.sql");
 		StringBuilder script = new StringBuilder();
@@ -60,6 +93,54 @@ public class EjecutableEjAglutinador {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public static void leeXML() throws JAXBException, SQLException, FileNotFoundException {
+		
+		// De XML a objetos Java
+        //Instanciamos el contexto JAXB indicando la clase que sería el RootElement
+        JAXBContext jC = JAXBContext.newInstance(ObjectFactory.class);
+        //Creamos un UnMarshaller en el contexto de la clase Examen
+        Unmarshaller jUnmarshaller = jC.createUnmarshaller();
+        
+        JAXBElement <TipoExamen> jaxbElement = (JAXBElement <TipoExamen>) jUnmarshaller.unmarshal(new FileInputStream("xml/examen.xml"));
+        
+        //Utilizamos el metodo unmarshall para obtener datos de un fichero (Reader)
+        TipoExamen miExamen = (TipoExamen) jaxbElement.getValue(); 
+        		
+        TipoAutores autores = miExamen.getAutores();
+        
+        List<TipoAutor> listaAutores=autores.getAutor();
+        
+        //Insert
+        String insertAutor = "INSERT INTO autor values(?,?,?,?,?,?)";
+        PreparedStatement sentencia = connection.prepareStatement(insertAutor);
+        
+        for(int i=0; i<listaAutores.size(); i++) {
+        	sentencia.setString(1, listaAutores.get(i).getId());
+        	sentencia.setString(2, listaAutores.get(i).getNombre());
+        	sentencia.setString(3, listaAutores.get(i).getApellido1());
+        	sentencia.setString(4, listaAutores.get(i).getApellido2());
+        	sentencia.setString(5, listaAutores.get(i).getPuesto());
+        	sentencia.setString(6, listaAutores.get(i).getEntidadTrabajo());
+        	sentencia.executeUpdate();
+        }
+        
+        TipoPreguntas preguntas = miExamen.getPreguntas();
+        
+        List<TipoPregunta> listaPreguntas=preguntas.getPregunta();
+        
+        //Insert
+        String insertPregunta = "INSERT INTO pregunta (dificultad, enunciado, idAutor) values(?,?,?)";
+        sentencia = connection.prepareStatement(insertPregunta);
+        
+        for(int i=0; i<listaPreguntas.size(); i++) {
+        	sentencia.setString(1, listaPreguntas.get(i).getDificultad().toString());
+        	sentencia.setString(2, listaPreguntas.get(i).getEnunciado());
+        	sentencia.setString(3, listaPreguntas.get(i).getAutoria());
+        	sentencia.executeUpdate();
+        }
+        
 	}
 	
 
